4abac3bc2053ed1fbe7c0d357752bf5d07eb47db
alber <reehanakaleel@gmail.com>
2024-11-11 15:36:34 -0500

    dummy

--- src/lab001.java
+++ src/lab001.java
@@ -1,5 +1,12 @@
 public class lab001 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        //primitive data type
+        byte b = 100; //8 bits , 1 byte
+        short s = 128; //16 bits, 2 byte
+        int i = 1000; //32 bits, 4 byte
+        long phone_number = 4343378344l;
+        System.out.println();
+        // 99% we use only int double;
     }
 }
