75b7e3e5c3114ccfd95207a4b11d28e4c1ee086e
alber <reehanakaleel@gmail.com>
2024-11-11 16:15:46 -0500

    task 08/10/24

---
+++ src/Task/task_04102024/task1.java
@@ -0,0 +1,33 @@
+package Task.task_04102024;
+
+public class task1 {public static void main(String[] args){
+    int table = 20;
+    String college = "Vels";
+    System.out.println();
+    System.out.printf("My college name is %s", college);
+    System.out.println();
+    System.out.println();
+    System.out.printf("%d * %d = %d", table, 1, table * 1 );
+    System.out.println();
+    System.out.printf("%d * %d = %d", table, 2, table * 2 );
+    System.out.println();
+    System.out.printf("%d * %d = %d", table, 3, table * 3 );
+    System.out.println();
+    System.out.printf("%d * %d = %d", table, 4, table * 4 );
+    System.out.println();
+    System.out.printf("%d * %d = %d", table, 5, table * 5 );
+    System.out.println();
+    System.out.printf("%d * %d = %d", table, 6, table * 6 );
+    System.out.println();
+    System.out.printf("%d * %d = %d", table, 7, table * 7 );
+    System.out.println();
+    System.out.printf("%d * %d = %d", table, 8, table * 8 );
+    System.out.println();
+    System.out.printf("%d * %d = %d", table, 9, table * 9 );
+    System.out.println();
+    System.out.printf("%d * %d = %d", table, 10, table * 10 );
+
+    //
+
+}
+}
