d544d0a635e2dc56ae016c5ce8336e791dff7249
alber <reehanakaleel@gmail.com>
2024-11-18 14:53:19 -0500

    task 08/10/24

---
+++ src/Task/task_10102024.java
@@ -0,0 +1,38 @@
+package Task;
+
+public class task_10102024 {
+    public static void main(String[] args){
+
+        //integer literals
+        int num1 = 24;
+        int num2 = 2222010;
+        //floating-point literals
+        float floatNumber = 55.2f;
+        double doubleNumber = 222.499-324d;
+        //character literals
+        char myChar = 'A';
+        char newline = '\n';
+        //String literals
+        String greeting = "HelloWorld";
+        //String nullLiteral
+        String noValue = null;
+        // Numeric Literal with Underscore
+        int largeNumber = 45000;
+        //Boolean literals
+        boolean isActive = true;
+        boolean isInactive = false;
+
+        System.out.println("Integer literal: "+ num1);
+        System.out.println("Floating point literal: "+ floatNumber);
+        System.out.println("Floating point literal2: "+ doubleNumber);
+        System.out.println("Char literal 1: "+ myChar);
+        System.out.println("Char literal Newline: "+ newline);
+        System.out.println("String literal: "+ greeting);
+        System.out.println("String no value: "+ null);
+        System.out.println("Numeric Literal with Underscore: "+ largeNumber);
+        System.out.println("Boolean literal1: "+ isActive);
+        System.out.println("Boolean literal2: "+ isInactive);
+
+
+    }
+}
